c97f9f99a6fea5cdaf7143e894c5524a18715036
m. zuhair .y <zuhairyuliansyah688@gmail.com>
2026-01-06 14:11:02 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://github.com/Zuhairx/My-Portfolio',
+  base: '/My-Portfolio/',
   plugins: [
     
     react(),
