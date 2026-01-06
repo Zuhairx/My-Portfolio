ae10dfb70ad031c994359f3488ff1d48aa1f323e
m. zuhair .y <zuhairyuliansyah688@gmail.com>
2026-01-06 14:00:51 +0700

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/My-Portfolio/',
+  base: 'https://github.com/Zuhairx/My-Portfolio',
   plugins: [
     
     react(),
